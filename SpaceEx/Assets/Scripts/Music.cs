49da7f7194858c778bd052fafebe83d0f2096bbd
YoungSensei <59657629+YoungSensei@users.noreply.github.com>
2020-02-04 19:11:17 -0500

    fixes and music

---
+++ SpaceEx/Assets/Scripts/Music.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    public AudioSource audi;
+    public AudioClip[] myMusic = new AudioClip[3];
+    // Use this for initialization
+    void Awake()
+    {
+        audi = GetComponent<AudioSource>();
+       // myMusic = Resources.LoadAll("Music", typeof(AudioClip)) as AudioClip[];
+        audi.clip = myMusic[0] as AudioClip;
+    }
+    void Start()
+    {
+
+        audi.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!audi.isPlaying)
+            playRandomMusic();
+    }
+    void playRandomMusic()
+    {
+        audi.clip = myMusic[Random.Range(0, myMusic.Length)] as AudioClip;
+        audi.Play();
+    }
+}
